3e510743700449c0edc426f1b6f136009339cc91
qiaoqiao-haiyang <1669008156@qq.com>
2020-02-19 15:49:56 +0800

    奥里给

---
+++ Webpack/webpack.config.js
@@ -0,0 +1,44 @@
+// node引入模块的方式
+const path = require("path");
+module.exports = {
+    mode:"development", //development|production
+    // 入口文件 
+    entry:"./src/index.js",
+    // 出口文件 打包到哪
+    output:{
+        filename:"bundle.js",
+        // path是绝对路径 
+        path:path.resolve(__dirname,"dist")
+    },
+    module:{
+        rules:[
+            {
+                test:/\.(jpg|png|gif)$/,
+                use:{
+                    loader:'url-loader',
+                    options:{
+                        outputPath:'images/',
+                        limit:100,
+                        name:'[name].[ext]'
+                    }
+                }
+            },
+            {
+                test:/\.css$/,
+                use:[ //loader的执行顺序从后向前
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test:/\.scss$/,
+                use:[ //loader的执行顺序从后向前
+                    "style-loader",
+                    "css-loader",
+                    // "postcss-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    }
+}
