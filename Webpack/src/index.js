cf9d07fd47903bc92c89cc737c76a4bcb66e7084
qiaoqiao-haiyang <1669008156@qq.com>
2020-02-25 17:57:56 +0800

    好多

--- Webpack/src/index.js
+++ Webpack/src/index.js
@@ -2,6 +2,12 @@
 import img from './img/24.jpg';
 // import style from './css/index.css';
 import style from './css/test.scss';
+let aaaaaa = 100;
+let fn=(x,y)=>{
+    console.log(x,y)
+}
+fn(1,2)
+console.log(aaaaaa);
 console.log('111111');
 var oImg = new Image();
 oImg.src= img;
--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -1,15 +1,16 @@
 // node引入模块的方式
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const webpack  = require('webpack');
+const output = require("./webpack.output.js");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = {
     mode:"development", //development|production
     // 入口文件 
     entry:"./src/index.js",
     // 出口文件 打包到哪
-    output:{
-        filename:"bundle.js",
-        // path是绝对路径 
-        path:path.resolve(__dirname,"dist")
-    },
+    output:output,
     module:{
         rules:[
             {
@@ -35,10 +36,40 @@ module.exports = {
                 use:[ //loader的执行顺序从后向前
                     "style-loader",
                     "css-loader",
-                    // "postcss-loader",
+                    "postcss-loader",
                     "sass-loader"
                 ]
+            },
+            {
+                test:/\.js$/,
+                loader: "babel-loader",
+                options:{
+                    presets:["@babel/preset-env"]
+                },
+                exclude: /node_modules/
             }
         ]
+    },
+    plugins:[
+        // template中的src/indx.html作为模板生成dist目录，并把打包生成的js注入到html中
+        new HtmlWebpackPlugin({
+            template:"./src/index.html"
+        }),
+        // 打包之前先将dist目录删除
+        new CleanWebpackPlugin(),
+        new webpack.BannerPlugin("唯创所有"),
+        new CopyWebpackPlugin([{
+            from:"./note.txt",
+            to:"./public"
+
+        }])
+    ],
+    devServer:{
+        contentBase:path.resolve(__dirname,"dist"),
+        host:'localhost',
+        open:true
+        // port:
     }
 }
+
+
