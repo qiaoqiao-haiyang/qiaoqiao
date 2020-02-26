b93515563ff517961ec96efaf9a9e3c20607f7d9
qiaoqiao-haiyang <1669008156@qq.com>
2020-02-26 18:07:26 +0800

    params/query/和直接传参三种方式

--- project/src/views/Detail.vue
+++ project/src/views/Detail.vue
@@ -1,6 +1,18 @@
 <template>
     <div>
         我是详情页
+        <!-- {{$route.params.id}}
+        {{$route.params.name}} -->
+
+
+<!--         
+         {{$route.query.id}}
+        {{$route.query.name}} -->
+
+
+        {{$route.params.id}}
+        {{$route.params.name}}
+        
     </div>
 </template>
 
